0bf87541f7c9bd1d686b8fa3cbc5924766f72c7d
Jaka Kordez <jaka.kordez@gmail.com>
2016-03-08 11:02:07 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function() {
 	//stran nalozena
+	var opomniki = document.getElementById("opomniki");
+	opomniki.setAttribute("class", "rob senca");
 	var txtUser = document.getElementById("uporabnisko_ime");
 	var gumb = document.getElementById("prijavniGumb");
 	var pokrivalo = document.getElementsByClassName("pokrivalo")[0];
@@ -9,7 +11,7 @@ window.addEventListener('load', function() {
 		p.innerHTML = txtUser.value;
 		pokrivalo.style.display = "none";
 	};
-	var opomniki = document.getElementById("opomniki");
+	
 	var btnDodajOpomnik = document.getElementById("dodajGumb");
 	btnDodajOpomnik.onclick = function(){
 		var txtNazivOpomnika = document.getElementById("naziv_opomnika");
@@ -19,7 +21,7 @@ window.addEventListener('load', function() {
 		txtNazivOpomnika.value = "";
 		txtTrajanjeOpomnika.value = "";
 		
-		opomniki.innerHTML = opomniki.innerHTML+"<div class='opomnik'><div class='naziv_opomnika'>"+naziv+"</div><div class='cas_opomnika'> Opomnik čez <span>"+trajanje+"</span> sekund.</div></div>";
+		opomniki.innerHTML = opomniki.innerHTML+"<div class='opomnik rob senca'><div class='naziv_opomnika'>"+naziv+"</div><div class='cas_opomnika'> Opomnik čez <span>"+trajanje+"</span> sekund.</div></div>";
 		
 	};
 		
@@ -31,7 +33,12 @@ window.addEventListener('load', function() {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
-	
+			
+			if(cas > 0) {
+				cas--;
+				casovnik.innerHTML = cas;
+				if(cas == 0) alert("Opomnik!\n\nZadolžitev "+opomnik.querySelector(".naziv_opomnika").innerHTML+" je potekla!");
+			}
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
