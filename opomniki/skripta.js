982ae7a1b9ea488665a5bbed0bfcdd4d9955d918
Jaka Kordez <jaka.kordez@gmail.com>
2016-03-08 10:40:01 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,11 +1,20 @@
 window.addEventListener('load', function() {
 	//stran nalozena
+	var txtUser = document.getElementById("uporabnisko_ime");
+	var gumb = document.getElementById("prijavniGumb");
+	var pokrivalo = document.getElementsByClassName("pokrivalo")[0];
+	gumb.onclick = function(){
+		var p = document.getElementById("uporabnik");
+		console.log(txtUser.value);
+		p.innerHTML = txtUser.value;
+		pokrivalo.style.display = "none";
+	};
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
